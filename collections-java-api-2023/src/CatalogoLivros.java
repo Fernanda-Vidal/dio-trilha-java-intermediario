178227569d31ec755172ecec08667f936e70d9be
Fernanda-Vidal <fevidal.dev@gmail.com>
2023-09-16 20:20:27 -0300

    Operações Básicas List - Pesquisa List

---
+++ collections-java-api-2023/src/CatalogoLivros.java
@@ -0,0 +1,69 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class CatalogoLivros {
+    private List<Livro> livrosList;
+
+    public CatalogoLivros() {
+        this.livrosList = new ArrayList<>();
+    }
+
+    public void adicionarLivro(String titulo, String autor, int anoPublicacao) {
+        livrosList.add(new Livro(titulo, autor, anoPublicacao));
+    }
+
+    public List<Livro> pesquisarPorAutor(String autor) {
+        List<Livro> livrosPorAutor = new ArrayList<>();
+        if (!livrosList.isEmpty()) {
+            for (Livro l : livrosList) {
+                if(l.getAutor().equalsIgnoreCase(autor)) {
+                    livrosPorAutor.add(l);
+                }
+            }
+        }
+        return livrosPorAutor;
+    }
+    
+    public List<Livro> pesquisarPorIntervaloAnos(int anoInicial, int anoFinal) {
+        List<Livro> livosPorIntervaloAnos = new ArrayList<>();
+        if (!livrosList.isEmpty()) {
+            for (Livro l : livrosList) {
+                if(l.getAnoPublicacao() >= anoInicial && l.getAnoPublicacao() <= anoFinal) {
+                    livosPorIntervaloAnos.add(l);
+                }
+            }
+        }
+        return livosPorIntervaloAnos;
+    }
+
+    public Livro pesquisarPorTitulo(String titulo) {
+        Livro livroPorTitulo = null;
+        if (!livrosList.isEmpty()) {
+            for (Livro l : livrosList) {
+                if(l.getTitulo().equalsIgnoreCase(titulo)) {
+                    livroPorTitulo = l;
+                    break;
+                }
+            }
+        }
+        return livroPorTitulo;
+    }
+
+    public static void main(String[] args) {
+        CatalogoLivros catalogoLivros = new CatalogoLivros();
+
+        catalogoLivros.adicionarLivro("O Senhor dos Anéis", "Tolkien", 1954);
+        catalogoLivros.adicionarLivro("David Cooperfield", "Charles Dickens", 1850);
+        catalogoLivros.adicionarLivro("O Mestre e a Margarida", "Mikhail Bulgakov", 1967);
+        catalogoLivros.adicionarLivro("O Rei do Inverno - Crônicas de Arthur", "Bernard Cornwell", 1995);
+        catalogoLivros.adicionarLivro("O Rei do Inverno - Crônicas de Arthur", "Bernard Cornwell", 1990);
+        catalogoLivros.adicionarLivro("O inimigo de Deus - Crônicas de Arthur", "Bernard Cornwell", 1996);
+        catalogoLivros.adicionarLivro("Excalibur - Crônicas de Arthur", "Bernard Cornwell", 1997);
+
+        System.out.println(catalogoLivros.pesquisarPorAutor("Mikhail Bulgakov"));
+        
+        System.out.println(catalogoLivros.pesquisarPorIntervaloAnos(1950, 1960));
+
+        System.out.println(catalogoLivros.pesquisarPorTitulo("O Rei do Inverno - Crônicas de Arthur"));
+    }
+}
---
+++ collections-java-api-2023/src/SomaNumero.java
@@ -0,0 +1,83 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class SomaNumero {
+    private List<Integer> numerosInteiros;
+
+    public SomaNumero() {
+        this.numerosInteiros = new ArrayList<>();
+    }
+
+    public void adicionarNumero(int numero) {
+        numerosInteiros.add(numero);
+    }
+
+    public int calcularSoma() {
+        int soma = 0;
+        for (int n : numerosInteiros) {
+            soma += n;
+        }
+        return soma;
+    }
+
+    public int encontrarMaiorNumero() {
+        int numero = 0;
+        if (!numerosInteiros.isEmpty()) {
+            numero = numerosInteiros.get(0);
+            for (int n : numerosInteiros) {
+                if (n > numero) {
+                    numero = n;
+                }
+            }
+        }
+        return numero;
+    }
+
+    public int encontrarMenorNumero() {
+         int numero = 0;
+        if (!numerosInteiros.isEmpty()) {
+            numero = numerosInteiros.get(0);
+            for (int n : numerosInteiros) {
+                if (n < numero) {
+                    numero = n;
+                }
+            }
+        }
+        return numero;
+    }
+
+    public void exibirNumeros() {
+        if (!numerosInteiros.isEmpty()) {
+            System.out.println(numerosInteiros);
+        } else {
+            System.out.println("A lista está vazia");
+        }
+    }
+
+    public static void main(String[] args) {
+        SomaNumero somaNumero = new SomaNumero();
+
+        somaNumero.exibirNumeros();
+        System.out.println("A soma dos números é: " + somaNumero.calcularSoma());
+
+        somaNumero.adicionarNumero(900);
+
+        somaNumero.adicionarNumero(8);
+        somaNumero.adicionarNumero(2);
+        
+        somaNumero.exibirNumeros();
+        System.out.println("A soma dos números é: " + somaNumero.calcularSoma());
+        
+        somaNumero.adicionarNumero(1);
+        somaNumero.adicionarNumero(2);
+        somaNumero.adicionarNumero(20);
+        somaNumero.adicionarNumero(900);
+        somaNumero.adicionarNumero(9);
+        somaNumero.adicionarNumero(11);
+        
+        somaNumero.exibirNumeros();
+        System.out.println("O maior número é o: " + somaNumero.encontrarMaiorNumero());
+        System.out.println("O menor número é o: " + somaNumero.encontrarMenorNumero());
+        
+    }
+}
